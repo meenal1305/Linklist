9925941c1fd007811e039c6723fd3b0074bf11fd
meenal1305 <100236356+meenal1305@users.noreply.github.com>
2022-09-03 20:40:23 +0530

    Create deletion2.cpp

---
+++ deletion2.cpp
@@ -0,0 +1,44 @@
+//deletion from end
+//insertion at first
+
+class node{
+    public:
+    int data;
+    node* next;
+    node* pre;
+    node (int x)
+    {
+      data=x;
+        next=NULL;
+        pre=NULL;
+    }
+};
+int main() {
+    node* head;
+    node* tail;
+    bool first=true;
+    int n; cin>>n;
+    while(n--)
+    {
+        int val; cin>>val;
+        node* temp=new node(val);
+        if(first==true)
+        {
+            head=temp;
+            tail=temp;
+            first=false;
+        }
+        else{
+            tail->next=temp;
+            temp->pre=tail;
+                tail=temp;
+        }
+    }
+   tail->pre->next=NULL;
+        tail=tail->pre;
+    while(head!=NULL)
+    {
+        cout<<head->data<<endl;
+        head=head->next;
+    }
+}
